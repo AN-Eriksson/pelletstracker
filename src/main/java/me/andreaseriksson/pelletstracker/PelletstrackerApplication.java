ee763ae5103e1a0ee9c643751e21da695396fa8f
Andreas Eriksson <ae225ak@student.lnu.se>
2025-07-08 09:25:28 +0200

    Initial commit

---
+++ src/main/java/me/andreaseriksson/pelletstracker/PelletstrackerApplication.java
@@ -0,0 +1,13 @@
+package me.andreaseriksson.pelletstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PelletstrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PelletstrackerApplication.class, args);
+    }
+
+}
