486da016308db7531f2474ed012820f2738de994
Andreas Eriksson <ae225ak@student.lnu.se>
2025-11-03 12:10:06 +0100

    Change Vite build output folder to backends static folder

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   build: {
+    outDir: '../src/main/resources/static', // Write build ouput to be served by Spring Boot
     sourcemap: false,
   },
   esbuild: {
